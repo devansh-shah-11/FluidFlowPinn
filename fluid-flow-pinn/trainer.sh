c337e6c2e68dd39ff690e8803404f05a54e3f670
Devansh Shah <devanshnshah11@gmail.com>
2026-04-25 10:33:14 -0400

    added sbatch trainer config

---
+++ fluid-flow-pinn/trainer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=fluid_pinn
+#SBATCH --output=./logs/%j_%x.out
+#SBATCH --error=./logs/%j_%x.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=dns5508@nyu.edu
+#SBATCH --partition=c12m85-a100-1
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --requeue
+
+mkdir -p ./logs
+
+singularity exec --bind /scratch --nv \
+  --overlay /scratch/dns5508/env/another__overlay-25GB-500K.ext3:ro \
+  /scratch/dns5508/ubuntu-20.04.3.sif \
+  /bin/bash -c "
+  source /ext3/miniconda3/etc/profile.d/conda.sh
+  conda activate cv
+  export PATH=/home/dns5508/.local/bin:\$PATH
+
+  cd /scratch/dns5508/FluidFlowPinn/fluid-flow-pinn
+
+  python train.py \
+    --config configs/default.yaml \
+    --fdst-path /scratch/dns5508/FDST_Data/ \
+    --epochs 20 \
+    --wandb-project fluid-pinn \
+    --wandb-entity dns5508
+"
